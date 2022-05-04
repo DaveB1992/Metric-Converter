e4e51cb997c14ff04945023f35e3ba0b1302bf40
Dave Bratby <69348013+DaveB1992@users.noreply.github.com>
2022-05-04 10:07:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const value = document.querySelector(`#value`);
+const length = document.querySelector(`#length`);
+const volume = document.querySelector(`#volume`);
+const weight = document.querySelector(`#weight`);
+const message = ` `;
+
+function renderText(){
+    if (message === ` `) {
+        return console.log(`The Message Is Blank`)
+    }
+};
+
+renderText()
