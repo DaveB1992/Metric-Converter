e266bf35e57d00bca707338760bc54808d40c763
Dave Bratby <69348013+DaveB1992@users.noreply.github.com>
2022-05-11 14:34:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const value = document.querySelector(`#value`);
-const length = document.querySelector(`#length`);
-const volume = document.querySelector(`#volume`);
-const weight = document.querySelector(`#weight`);
-const message = ` `;
+function aRound(n) {
+    return Math.round((n + Number.EPSILON) * 100)
+    /100;
+};
 
 function renderText(){
-    if (message === ` `) {
-        return console.log(`The Message Is Blank`)
-    }
+
+    const value = document.querySelector(`#value`).value
+
+    document.querySelector(`#lengthM`).textContent = "Feet(FT) to Meters(M): " + 
+    aRound(value/3.281);
+    document.querySelector(`#lengthFT`).textContent = "Meters(M) to Feet(FT): " + 
+    aRound(value*3.281);
+
+    document.querySelector(`#volumeL`).textContent = "Litres(L) to Gallons(G): " +
+    aRound(value / 4.546);
+    document.querySelector(`#volumeG`).textContent = "Gallons(G) to Litres(L): " +
+    aRound(value * 4.546);
+
+    document.querySelector(`#weightP`).textContent = "Pounds(lbs) to Kilograms(Kgs): " +
+    aRound(value / 2.205);
+    document.querySelector(`#weightkg`).textContent = "Kilograms(Kgs) to Pounds(Lbs): " +
+    aRound(value * 2.205);
+
 };
+
 
 renderText()
